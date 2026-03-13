703ee0d2a69758ef585ec036ecec4f92d6430fef
salendrisiri <b23cn032@kitsw.ac.in>
2026-03-13 19:54:36 +0530

    Faculty

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const facultyRoutes = require("./routes/facultyRoutes");
+
+const app = express();
+app.use(bodyParser.json());
+
+// MongoDB connection
+mongoose.connect("mongodb://127.0.0.1:27017/facultyDB")
+.then(()=>console.log("MongoDB Connected"))
+.catch(err=>console.log(err));
+
+// Routes
+app.use("/api", facultyRoutes);
+
+// Server
+app.listen(4000, ()=>{
+    console.log("Faculty Server running on port 4000");
+});
